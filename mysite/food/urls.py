5f70ab6b2f466e3751e459755f907db54cef3904
Bilal Saifi <52230954+bilalsaif@users.noreply.github.com>
2024-03-26 00:11:16 +0530

    Add files via upload

---
+++ mysite/food/urls.py
@@ -0,0 +1,22 @@
+from . import views
+from django.urls import path
+
+app_name='food'
+
+urlpatterns = [
+    path('',views.IndexClassView.as_view(), name='index'),
+    # path('<int:item_id>/',views.detail,name='detail'),
+    path('<int:pk>/',views.FoodDetail.as_view(),name='detail'),
+    # path('item/',views.item,name='item'),
+    # path('login/',views.login,name='login'),
+
+    # add item
+    path('add/',views.CreateItem.as_view(),name='create_item'),
+
+    # edit
+    path('update/<int:id>/',views.update_item,name='update_item'),
+    
+    # delete
+    path('delete/<int:id>/',views.delete_item,name='delete_item'),
+
+]
